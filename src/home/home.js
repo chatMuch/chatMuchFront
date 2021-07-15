99ca1e77afcabcd7f3d44733376102c6dad6853a
Cullen Sharp <cullen.sharp100@gmail.com>
2021-07-14 19:32:49 -0700

    State persists accross page changes

--- src/home/home.js
+++ src/home/home.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 
 // 3rd party resources
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
@@ -11,15 +11,7 @@ const axios = require('axios');
 import './home.scss';
 
 
-function Homepage({ socket, user, setUser }) {
-  const [username, setusername] = useState('');
-  const [password, setpassword] = useState('');
-  const [roomname, setroomname] = useState('');
-  
-
-  
-  //activates joinRoom function defined on the backend 
-
+function Homepage({ socket, setUser, password, setPassword, username, setUsername, roomName, setRoomName }) {
   const sendData = () => {
 
     // remove after testing
@@ -32,15 +24,14 @@ function Homepage({ socket, user, setUser }) {
     axios.post('http://localhost:3000/signup', signUpInfo)
       .then( function(response) {
         setUser(response.data.user);
-        console.log(user);
       })
       .catch( function(err) {
         console.log(err);
       });
 
-    if (username !== '' && roomname !== '') {
-      console.log(username, roomname);
-      socket.emit('joinRoom', { username, roomname });
+    if (username !== '' && roomName !== '') {
+      console.log(username, roomName);
+      socket.emit('joinRoom', { username, roomName });
 
       //if empty error message pops up and returns to the same page
     } else {
@@ -53,21 +44,21 @@ function Homepage({ socket, user, setUser }) {
     <div>
       <div className='homepage'>
         <h1 id='colorH1' style={{background: 'black', color:'white'}}>chatMuch</h1>
-        <input style={{marginLeft: '10%'}} placeholder='Input your username' value={username} onChange={(e) => setusername(e.target.value)}/>
-        <input style={{marginLeft: '10%'}} placeholder='Input your password' value={password} onChange={(e) => setpassword(e.target.value)}/>
-        {/* <input placeholder='Input the room name' value={roomname} onChange={(e) => setroomname(e.target.value)} /> */}
+        <input style={{marginLeft: '10%'}} placeholder='Input your username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <input style={{marginLeft: '10%'}} placeholder='Input your password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        {/* <input placeholder='Input the room name' value={roomName} onChange={(e) => setroomName(e.target.value)} /> */}
         <DropdownButton
           align="end"
           title="Rooms"
           id="dropdown-menu-align-right"
         >
-          <Dropdown.Item value={roomname} onSelect={() => setroomname('1')}>Room1</Dropdown.Item>
-          <Dropdown.Item value={roomname} onSelect={() => setroomname('2')}>Room2</Dropdown.Item>
-          <Dropdown.Item value={roomname} onSelect={() => setroomname('3')}>Room3</Dropdown.Item>
+          <Dropdown.Item value={roomName} onSelect={() => setRoomName('1')}>Room1</Dropdown.Item>
+          <Dropdown.Item value={roomName} onSelect={() => setRoomName('2')}>Room2</Dropdown.Item>
+          <Dropdown.Item value={roomName} onSelect={() => setRoomName('3')}>Room3</Dropdown.Item>
           <Dropdown.Divider />
         </DropdownButton>
         {/* Add third placeholder for password (option to remove) */}
-        <Link to={`/chat/${roomname}/${username}`}>
+        <Link to={`/chat/${roomName}/${username}`}>
           <button onClick={sendData}>Join</button>
         </Link>
       </div>
