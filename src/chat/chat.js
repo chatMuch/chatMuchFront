42ffb93a54cf9fa9107d33fffc5ab357ed43ca23
sunny-lee3 <sunnylee3712@gmail.com>
2021-07-15 22:26:59 -0700

    updated into MVP

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect, useRef } from 'react';
 
 // Styling resources
 import './chat.scss';
-
+import Clock from 'react-live-clock';
 //gets the data from the action object and reducers defined earlier
 function Chat({ username, roomName, socket }) {
   const [text, setText] = useState('');
@@ -45,44 +45,53 @@ function Chat({ username, roomName, socket }) {
   console.log(messages, 'mess');
 
   return (
-    <div className='chat'>
-      <div className='user-name'>
-        <h2>
-          {username} <span style={{ fontSize: '0.7rem' }}>in room { roomName }</span>
-        </h2>
+    <div>
+      <div>
+        <h1>DashMessenger</h1>
       </div>
-      <div className='chat-message'>
-        {messages.map((i) => {
-          if (i.username === username) {
-            return (
-              <div className='message'>
-                <p>{i.text}</p>
-                <span>{i.username}</span>
-              </div>
-            );
-          } else {
-            return (
-              <div className='message mess-right'>
-                <p>{i.text} </p>
-                <span>{i.username}</span>
-              </div>
-            );
-          }
-        })}
-        <div ref={messagesEndRef} />
-      </div>
-      <div className='send'>
-        <input
-          placeholder='Message'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              sendData();
-            }
-          }}
-        ></input>
-        <button onClick={sendData}>Send</button>
+      <div className='chatUpper'>
+      
+        <div className='chat'>
+          <div className='user-name'>
+            <h2>
+              {username} <span style={{ fontSize: '0.7rem' }}>in room { roomName }</span>
+            </h2>
+          </div>
+          <div className='chat-message'>
+            {messages.map((i) => {
+              if (i.username === username) {
+                return (
+                  <div className='message'>
+                    <p>{i.text}</p>
+                    <span>{i.username}</span>
+                  </div>
+                );
+              } else {
+                return (
+                  <div className='message mess-right'>
+                    <p>{i.text} </p>
+                    <span>{i.username}</span>
+                  </div>
+                );
+              }
+            })}
+            <div ref={messagesEndRef} />
+          </div>
+          <div className='send'>
+            <input
+              placeholder='Message'
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  sendData();
+                }
+              }}
+            ></input>
+            <button onClick={sendData}>Send</button>
+          </div>
+        </div>
+  
       </div>
     </div>
   );
